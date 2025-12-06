5fc991ef35f07ddd45a4a563eaa0767d04c4f814
Jeyasurya14 <jeyas865@gmail.com>
2025-12-06 21:43:55 +0530

    first commit

--- components/CreateEventModal.js
+++ components/CreateEventModal.js
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { min } from "moment" // Not used, removing
+
 import { X } from "lucide-react"
 
 export function CreateEventModal({ isOpen, onClose, onCreated }) {
